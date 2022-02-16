6a11b66af7fc7463ad5abb4d68527a4160f2f5a3
Jono Kemball <Noojuno@users.noreply.github.com>
2022-02-16 15:09:37 +1300

    Add better styling

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -2,18 +2,33 @@ import styles from "./Word.module.scss";
 
 type ReactElement = JSX.Element | JSX.Element[] | string | string[];
 
+const actualWord = "RENTS";
+
 export function Word({ children }: { children?: string }) {
   const word = children || "     ";
 
   return (
     <div className={styles.word}>
       {word.split("").map((l, i) => {
-        return <Letter key={i}>{l}</Letter>;
+        return (
+          <Letter actualWord={actualWord} index={i} key={i}>
+            {l}
+          </Letter>
+        );
       })}
     </div>
   );
 }
 
-function Letter({ children }: { children: ReactElement }) {
-  return <div className={styles.letter}>{children}</div>;
+type LetterState = "correct" | "close" | "incorrect" | "none";
+
+function Letter({ index, actualWord, children }: { index: number; actualWord: string; children: string }) {
+  const hasLetter = actualWord.includes(children);
+  const state: LetterState = children === " " ? "none" : hasLetter ? (actualWord[index] === children ? "correct" : "close") : "incorrect";
+
+  return (
+    <div className={styles.letter} data-state={state}>
+      {children}
+    </div>
+  );
 }
