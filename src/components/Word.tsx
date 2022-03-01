230a7c89cf486b19ed95cf82d0de54a2731a7742
Jono Kemball <Noojuno@users.noreply.github.com>
2022-03-02 03:01:23 +1300

    Add letter reveal animation

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,5 +1,8 @@
+import { useEffect, useState } from "react";
 import { getGuessStates, LetterState } from "../lib/guesses";
 import styles from "./Word.module.scss";
+
+const ANIMATION_DELAY = 0.4;
 
 interface WordProps {
   word?: string;
@@ -7,14 +10,23 @@ interface WordProps {
   revealed?: boolean;
 }
 
+// TODO: Letter reveal currently uses state, which is fine but I would rather it not or at least have state in game board
 export function Word({ word = "", solution, revealed }: WordProps) {
+  const [animate, setAnimate] = useState<boolean>(true);
+
   const guess = word + " ".repeat(solution.length - word.length);
   const states = getGuessStates(solution, guess);
+
+  useEffect(() => {
+    if (!revealed) return;
+
+    setTimeout(() => setAnimate(false), word.length * ANIMATION_DELAY * 1000);
+  }, [revealed, word]);
 
   return (
     <div className={styles.word}>
       {guess.split("").map((letter, i) => {
-        return <Letter key={i} letter={letter} state={states[i]} revealed={revealed} />;
+        return <Letter key={i} letter={letter} state={states[i]} revealed={revealed} delay={animate ? i * ANIMATION_DELAY : 0} />;
       })}
     </div>
   );
@@ -24,11 +36,15 @@ interface LetterProps {
   letter: string;
   revealed?: boolean;
   state: LetterState;
+  delay?: number;
 }
 
-function Letter({ letter, revealed, state }: LetterProps) {
+function Letter({ letter, revealed, state, delay = 0 }: LetterProps) {
+  const delayStyle = { animationDelay: `${delay}s`, transitionDelay: `${delay}s` };
+  const noTransitionStyle = { transition: "none" };
+
   return (
-    <div className={styles.letter} data-state={revealed ? state : "none"}>
+    <div className={styles.letter} data-state={revealed ? state : "none"} style={!revealed || delay <= 0 ? noTransitionStyle : delayStyle}>
       {letter}
     </div>
   );
